f8252ebdde60b09999ea70b02bbb82a278c99290
lephorx <ramonzielke09@gmail.com>
2026-05-25 15:24:45 +0200

    feat: canFlip und flip implementieren mit unittests

--- knobelscheit.ts
+++ knobelscheit.ts
@@ -1,11 +1,21 @@
 export class Knobelscheit {
-    private flipped: Set<number>
+  private flipped: Set<number>;
 
-    constructor() {
-        this.flipped = new Set();
-    }
+  constructor() {
+    this.flipped = new Set();
+  }
 
-    getActive(): number[] {
-        return [1, 2, 3, 4, 5, 6, 7, 8, 9].filter((n) => !this.flipped.has((n)))
+  getActive(): number[] {
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9].filter((n) => !this.flipped.has(n));
+  }
+
+  canFlip(numbers: number[]): boolean {
+    return numbers.every((n) => !this.flipped.has(n));
+  }
+
+  flip(numbers: number[]): void{
+    for(const n of numbers){
+        this.flipped.add(n);
     }
+  }
 }
--- knobelscheit_test.ts
+++ knobelscheit_test.ts
@@ -1,8 +1,24 @@
 import { assert, assertEquals } from "@std/assert";
-import { Knobelscheit } from "./knobelscheit.ts"
+import { Knobelscheit } from "./knobelscheit.ts";
 
 Deno.test("Beginnen mit allen 9 Zahlen", () => {
+  const brett = new Knobelscheit();
+  assertEquals(brett.getActive(), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+});
 
-    const brett = new Knobelscheit()
-    assertEquals(brett.getActive(), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+Deno.test("canFlip: aktive zahlen können umgeklappt werden", () => {
+  const brett = new Knobelscheit();
+  assert(brett.canFlip([1, 2, 3]));
+});
+
+Deno.test("canFlip: umgeklappte Zahlen können nicht erneut umgeklappt werden", () => {
+    const brett = new Knobelscheit();
+    brett.flip([1]);
+    assert(!brett.canFlip([1]));
+})
+
+Deno.test("flip: Zahlen werden markiert als umgeklappt", () =>{
+    const brett = new Knobelscheit();
+    brett.flip([1, 2]);
+    assertEquals(brett.getActive(), [3, 4, 5, 6, 7, 8, 9]);
 })
